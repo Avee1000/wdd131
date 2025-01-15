951bfde0b9dafcd6f8ad9eded08a1302d6feb4ba
Avee1000 <eddyidahosa01@gmail.com>
2025-01-15 12:18:08 +0100

    Added code for temples.html

---
+++ scripts/temples.js
@@ -0,0 +1,10 @@
+const modification = new Date(document.lastModified);
+
+const lastModified = document.querySelector('#lastModified');
+lastModified.innerHTML += modification;
+
+const hamburger = document.querySelector('#menu');
+const navigation = document.querySelector('nav');
+hamburger.addEventListener('click', function () {
+    document.body.classList.toggle('show');
+})

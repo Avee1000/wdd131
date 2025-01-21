4c1b7863e604bfc760d0ecbc039eec44e04abc64
Avee1000 <eddyidahosa01@gmail.com>
2025-01-21 14:15:47 +0100

    Add Singapore place page with weather data

---
+++ scripts/place.js
@@ -0,0 +1,9 @@
+const temperature = document.querySelector("#temperature");
+const condition = document.querySelector("#conditions");
+const wind = document.querySelector("#wind");
+const windChill = document.querySelector("#windchill");
+
+temperature.innerHTML = `<strong>Temperature:</strong> 10°C`;
+condition.innerHTML = `<strong>Conditions:</strong> Sunny`;
+wind.innerHTML = `<strong>Wind:</strong> 10 km/h`;
+windChill.innerHTML = `<strong>Wind Chill:</strong> 10°C`;

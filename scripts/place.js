e2277af2cf00423ecff1f412c7d841415fbd26cf
Avee1000 <eddyidahosa01@gmail.com>
2025-01-22 12:02:21 +0100

    Update Singapore page with new hero image, layout, and footer

--- scripts/place.js
+++ scripts/place.js
@@ -7,3 +7,15 @@ temperature.innerHTML = `<strong>Temperature:</strong> 10°C`;
 condition.innerHTML = `<strong>Conditions:</strong> Sunny`;
 wind.innerHTML = `<strong>Wind:</strong> 10 km/h`;
 windChill.innerHTML = `<strong>Wind Chill:</strong> 10°C`;
+
+
+const date = new Date;
+const year = document.getElementById('currentyear');
+
+year.innerHTML += date.getFullYear();
+
+/////////////////////////////////////////////////////////
+
+const modified = document.getElementById('lastModified');
+let lastModified = new Date(document.lastModified);
+modified.innerHTML += lastModified;

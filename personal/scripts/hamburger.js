4b4894cef2168785baa5ab2ec2c981bf28017770
Avee1000 <eddyidahosa01@gmail.com>
2025-02-12 18:46:40 +0100

    Update navigation and hero section styles

--- personal/scripts/hamburger.js
+++ personal/scripts/hamburger.js
@@ -5,6 +5,7 @@ const nav = document.querySelector('nav');
 hamburger.addEventListener('click', () => {
     nav.classList.toggle("show");
     nav.classList.remove('delete-span');
+    document.body.style.overflow = 'hidden';
 });
 
 document.querySelector(".delete").addEventListener("click", () => {

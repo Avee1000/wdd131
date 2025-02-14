ddc2de75cfa18fdcc4df921deed8e5af11786c21
Avee1000 <ida23001@byupathway.edu>
2025-02-14 10:18:34 +0100

    Add recent services feature

--- personal/scripts/sidenav.js
+++ personal/scripts/sidenav.js
@@ -67,6 +67,16 @@ document.getElementById('sideMenu').addEventListener('click', () => {
         document.getElementById('serviceContainer').append(comingSoon);
     });
 
+    document.querySelectorAll('.serviceNames').forEach((service) => { 
+        service.addEventListener('click', (e) => {
+            let storedServices = JSON.parse(localStorage.getItem('services')) || [];
+            if (!storedServices.includes(e.target.textContent)) {
+                storedServices.push(e.target.textContent);
+            }
+            localStorage.setItem('services', JSON.stringify(storedServices));
+        })
+    });
+
 });
 
 document.querySelector('.close').addEventListener('click', () => {
@@ -75,11 +85,46 @@ document.querySelector('.close').addEventListener('click', () => {
     document.querySelector('.just').style.display = "";
     document.querySelector('.close').style.display = "";
     //serviceUnorderedList.innerHTML = '';
-  //  comingSoon.innerHTML = '';
-
+    //  comingSoon.innerHTML = '';
+    
+    
 });
 
+const recentsh2 = document.querySelector('.recentContainer section h2');
+const recentUl = document.createElement('ul');
+document.querySelector(".recent").addEventListener('click', () => { 
+    
+    document.querySelector(".recentContainer").style.right = '0';
+    document.body.style.overflowY = "hidden";
+    document.querySelector('.justrecent').style.display = "block";
 
+    if (JSON.parse(localStorage.getItem('services') === null)) {
+        recentsh2.innerHTML = 'No services selected yet!';
+        recentsh2.style.fontSize = "25px";
+        recentsh2.style.fontWeight = "400";
+
+    } else {
+        if (recentUl) {
+            recentUl.innerHTML = '';
+        }
+        recentsh2.innerHTML = 'Your Recents';
+        let recentServices = JSON.parse(localStorage.getItem('services'));
+        recentServices.forEach((service) => {
+            const recentList = document.createElement('li');
+            recentList.innerHTML = service;
+            recentList.setAttribute('class', 'recentServices');
+            recentUl.appendChild(recentList);
+            document.querySelector('.recentContainer section').append(recentUl);
+            
+        });
+    }
+
+    document.querySelector(".justrecent").addEventListener('click', () => { 
+        document.querySelector(".recentContainer").style.right = '';
+        document.body.style.overflowY = "";
+        document.querySelector('.justrecent').style.display = "";
+    });
+});
 // Apply styles to all elements
 
 export default services;
